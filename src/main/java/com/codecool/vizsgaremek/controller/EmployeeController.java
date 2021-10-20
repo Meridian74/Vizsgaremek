7bed1be9d314b1a26beba3fcec35f848a61f77d6
Meridian74 <meridian@freemail.hu>
2021-10-20 20:20:24 +0200

    Add annotation on the employe controller

--- src/main/java/com/codecool/vizsgaremek/controller/EmployeeController.java
+++ src/main/java/com/codecool/vizsgaremek/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import com.codecool.vizsgaremek.dto.CreateEmployeeCommand;
 import com.codecool.vizsgaremek.dto.EmployeeDTO;
 import com.codecool.vizsgaremek.dto.UpdateEmployeeCommand;
 import com.codecool.vizsgaremek.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -24,16 +26,20 @@ public class EmployeeController {
 
    @PostMapping
    @ResponseStatus(HttpStatus.CREATED)
+   @Operation(summary = "Creates an employee")
+   @ApiResponse(responseCode = "201", description = "Employee has been created")
    public EmployeeDTO createEmployee(@RequestBody CreateEmployeeCommand command) {
       return employeeService.createEmployee(command);
    }
 
    @GetMapping("/{id}")
+   @Operation(summary = "find and get an employee by its id")
    public EmployeeDTO findEmployeeById(@PathVariable("id") long id) {
       return employeeService.findEmployeeById(id);
    }
 
    @PutMapping("/{id}")
+   @Operation(summary = "Updating and an employee by its id and new datas")
    public EmployeeDTO updateEmployee(
          @PathVariable("id") long id,
          @RequestBody UpdateEmployeeCommand command) {
@@ -42,11 +48,14 @@ public class EmployeeController {
 
    @DeleteMapping("/{id}")
    @ResponseStatus(HttpStatus.NO_CONTENT)
+   @Operation(summary = "Delete an employee by given its id")
+   @ApiResponse(responseCode = "204", description = "Employee has been deleted")
    public void deleteEmployee(@PathVariable("id") long id) {
       employeeService.deleteEmployeeById(id);
    }
 
    @GetMapping
+   @Operation(summary = "List all employee filtered with optional prefix string")
    public List<EmployeeDTO> employeeList(@RequestParam Optional<String> prefix) {
       return employeeService.employeeList(prefix);
    }
--- src/main/java/com/codecool/vizsgaremek/dto/CreateEmployeeCommand.java
+++ src/main/java/com/codecool/vizsgaremek/dto/CreateEmployeeCommand.java
@@ -1,5 +1,6 @@
 package com.codecool.vizsgaremek.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Name and birth date for the new employee", example = "Teszt Jakab, 1998-03-01")
 public class CreateEmployeeCommand {
 
    private String name;
